4f8938efcbef8417f68c19f070dc5454f97e0ef5
mikydees <mikydees2@gmail.com>
2022-10-18 06:07:53 +0000

    final copy

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big", "huge"];
+let noun = ["lion", "racoon", "tiger"];
+let ext = [".com", ".net"];
+
+function randomgenerator(multiply) {
+  return Math.floor(Math.random() * multiply.length);
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML =
+    pronoun[randomgenerator(pronoun)] +
+    adj[randomgenerator(adj)] +
+    noun[randomgenerator(noun)] +
+    ext[randomgenerator(ext)];
+
+  console.log("Hello Rigo!");
 };
